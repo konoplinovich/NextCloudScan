3abaf5cc8655abfc15fc1875ad98270ed8b24de8
Nick Bogdanov <konoplinovich@gmail.com>
2020-01-15 01:05:53 +0300

    added per-file test payload

---
+++ NCSPerFilePayload/NCSPerFilePayload.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NCSPerFilePayload
+{
+    class NCSPerFilePayload
+    {
+        private static string _fileName;
+        
+        static void Main(string[] args)
+        {
+            if (args.Length == 0) return;
+            _fileName = args[0];
+
+            if (!File.Exists(_fileName))
+            {
+                Console.WriteLine("[E]File not found");
+            }
+            else
+            {
+                try
+                {
+                    FileInfo fi = new FileInfo(_fileName);
+                    if (fi != null)
+                    {
+                        Console.WriteLine($"[D]{fi.Directory} [F]{fi.Name} [Size]{fi.Length} [LWT]{fi.LastWriteTime} [LAT]{fi.LastAccessTime}");
+                    }
+                }
+                catch (UnauthorizedAccessException e)
+                {
+                    Console.WriteLine($"[E]Access to {_fileName} is denied. {e.Message}");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"[E]Error. {e.Message}");
+                }
+            }
+        }
+    }
+}
