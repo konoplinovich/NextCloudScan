ed8a8b1cc2e3e6e7f47d8c58eb3aaed7a848d95b
Nick Bogdanov (work) <konoplinovich@gmail.com>
2020-02-07 16:33:01 +0300

    added CommandLineParser (NuGet) and comandline options
    https://github.com/commandlineparser/commandline

---
+++ NextCloudScanStatsView/Options.cs
@@ -0,0 +1,19 @@
+﻿using CommandLine;
+
+namespace NextCloudScanStatsView
+{
+    partial class StatsViewer
+    {
+        internal class Options
+        {
+            [Option('f', "file", Required = true, HelpText = "Statistics XML files to be processed")]
+            public string InputFile { get; set; }
+
+            [Option('s', "sessions", Required = false, HelpText = "Show last <n> sessions")]
+            public int Lines { get; set; }
+
+            [Option('t', "total", Required = false, HelpText = "Summary only")]
+            public bool SummaryOnly { get; set; }
+        }
+    }
+}
--- NextCloudScanStatsView/StatsViewer.cs
+++ NextCloudScanStatsView/StatsViewer.cs
@@ -1,78 +1,140 @@
-﻿using NextCloudScan.Statistics.Lib;
+﻿using CommandLine;
+using CommandLine.Text;
+using NextCloudScan.Statistics.Lib;
 using System;
 using System.Collections.Generic;
 
 namespace NextCloudScanStatsView
 {
-    class StatsViewer
+    partial class StatsViewer
     {
         private static string _statsFile;
-        
+        private static int _lines;
+        private static bool _summaryOnly;
+        private static long _added = 0;
+        private static long _removed = 0;
+        private static long _affected = 0;
+        private static long _scanTime = 0;
+        private static long _fileScanTime = 0;
+        private static long _folderScanTime = 0;
+
         static void Main(string[] args)
         {
             if (args.Length == 0) return;
-            _statsFile = args[0];
+
+            var parser = new Parser(with => with.HelpWriter = null);
+            var parserResult = parser.ParseArguments<Options>(args);
+            parserResult.WithParsed<Options>(options => RunOptions(options)).WithNotParsed(errs => DisplayHelp(parserResult, errs));
 
             StatisticsAgregator agregator = new StatisticsAgregator(_statsFile);
+            if (!agregator.Successfully) return;
 
             List<SessionStatistics> statistics = new List<SessionStatistics>();
             if (agregator.Successfully) statistics = agregator.Statistisc;
 
-            Console.WriteLine("────────────────────────────────────┬──────────────────────┬──────────┬───────────┬───────────┬───────┬───────┬───────────┬───────────────");
-            Console.WriteLine("Id                                  │            Start Time│     Total│       Scan│        [+]│    [-]│    [A]│      Files│        Folders");
-            Console.WriteLine("────────────────────────────────────┼──────────────────────┼──────────┼───────────┼───────────┼───────┼───────┼───────────┼───────────────");
+            CalculateSummary(statistics);
+            if (!_summaryOnly) ShowSessions(statistics, _lines);
+            ShowSummary(agregator, statistics);
+        }
 
-            long added = 0;
-            long removed = 0;
-            long affected = 0;
-            long scanTime = 0;
-            long fileScanTime = 0;
-            long folderScanTime = 0;
-
-            foreach (SessionStatistics stat in statistics)
-            {
-                TimeSpan scanElapsedTime = TimeSpan.FromTicks(stat.ScanElapsedTime);
-                TimeSpan fileProcessingElapsedTime = TimeSpan.FromTicks(stat.FileProcessingElapsedTime);
-                TimeSpan folderProcessingElapsedTime = TimeSpan.FromTicks(stat.FolderProcessingElapsedTime);
-
-                added += stat.AddedFiles;
-                removed += stat.RemovedFiles;
-                affected += stat.AffectedFolders;
-
-                scanTime += stat.ScanElapsedTime;
-                fileScanTime += stat.FileProcessingElapsedTime;
-                folderScanTime += stat.FolderProcessingElapsedTime;
-
-                Console.WriteLine($"{stat.Id}│\t{stat.StartTime}│\t{stat.TotalFiles,6}│\t{scanElapsedTime.TotalSeconds,10:0.0000}│\t{stat.AddedFiles,6}│\t{stat.RemovedFiles,6}│\t{stat.AffectedFolders,6}│\t{fileProcessingElapsedTime.TotalSeconds,10:0.0000}│\t{folderProcessingElapsedTime.TotalSeconds,10:0.0000}");
-            }
-
-            Console.WriteLine("────────────────────────────────────┴──────────────────────┴──────────┴───────────┴───────────┴───────┴───────┴───────────┴───────────────");
-
+        private static void ShowSummary(StatisticsAgregator agregator, List<SessionStatistics> statistics)
+        {
             DateTime first = statistics[0].StartTime;
-            DateTime last = statistics[statistics.Count-1].StartTime;
+            DateTime last = statistics[statistics.Count - 1].StartTime;
             TimeSpan period = last - first;
 
             double hoursAll = period.TotalHours;
-            double hoursWork = (TimeSpan.FromTicks(scanTime) + TimeSpan.FromTicks(fileScanTime) + TimeSpan.FromTicks(folderScanTime)).TotalHours;
+            double hoursWork = (TimeSpan.FromTicks(_scanTime) + TimeSpan.FromTicks(_fileScanTime) + TimeSpan.FromTicks(_folderScanTime)).TotalHours;
             double ratio = hoursWork / hoursAll;
 
             Console.WriteLine("");
             Console.WriteLine($"Statistics period:      {ToReadableString(period)}");
             Console.WriteLine($"First session:          {statistics[0].StartTime}");
             Console.WriteLine($"Last session start:     {statistics[statistics.Count - 1].StartTime}");
-            Console.WriteLine($"Sessions count:         {statistics.Count} (average interval {(period.TotalMinutes/statistics.Count):0.000} min)");
+            Console.WriteLine($"Sessions count:         {statistics.Count} (average interval {(period.TotalMinutes / statistics.Count):0.000} min)");
             Console.WriteLine("");
-            Console.WriteLine($"Files added/removed:    {added}/{removed}");
-            Console.WriteLine($"Processed folders:      {affected}");
+            Console.WriteLine($"Files added/removed:    {_added}/{_removed}");
+            Console.WriteLine($"Processed folders:      {_affected}");
             Console.WriteLine("");
-            Console.WriteLine($"Scan time:              {ToReadableString(TimeSpan.FromTicks(scanTime))}");
-            Console.WriteLine($"File processing time:   {ToReadableString(TimeSpan.FromTicks(fileScanTime))}");
-            Console.WriteLine($"Folder processing time: {ToReadableString(TimeSpan.FromTicks(folderScanTime))}");
-            Console.WriteLine($"Total work time:        {ToReadableString(TimeSpan.FromTicks(scanTime) + TimeSpan.FromTicks(fileScanTime) + TimeSpan.FromTicks(folderScanTime))}");
+            Console.WriteLine($"Scan time:              {ToReadableString(TimeSpan.FromTicks(_scanTime))}");
+            Console.WriteLine($"File processing time:   {ToReadableString(TimeSpan.FromTicks(_fileScanTime))}");
+            Console.WriteLine($"Folder processing time: {ToReadableString(TimeSpan.FromTicks(_folderScanTime))}");
+            Console.WriteLine($"Total work time:        {ToReadableString(TimeSpan.FromTicks(_scanTime) + TimeSpan.FromTicks(_fileScanTime) + TimeSpan.FromTicks(_folderScanTime))}");
             Console.WriteLine($"Ratio (work/period):    {ratio:0.0000}");
+            Console.WriteLine("---");
             Console.WriteLine($"Statistics file size:   {agregator.Size} bytes");
-
+            Console.WriteLine("");
         }
+
+        private static void CalculateSummary(List<SessionStatistics> statistics)
+        {
+            foreach (SessionStatistics stat in statistics)
+            {
+                TimeSpan scanElapsedTime = TimeSpan.FromTicks(stat.ScanElapsedTime);
+                TimeSpan fileProcessingElapsedTime = TimeSpan.FromTicks(stat.FileProcessingElapsedTime);
+                TimeSpan folderProcessingElapsedTime = TimeSpan.FromTicks(stat.FolderProcessingElapsedTime);
+
+                _added += stat.AddedFiles;
+                _removed += stat.RemovedFiles;
+                _affected += stat.AffectedFolders;
+
+                _scanTime += stat.ScanElapsedTime;
+                _fileScanTime += stat.FileProcessingElapsedTime;
+                _folderScanTime += stat.FolderProcessingElapsedTime;
+            }
+        }
+
+        private static void ShowSessions(List<SessionStatistics> statistics, int lines)
+        {
+            Console.WriteLine();
+            Console.WriteLine($"Show last {lines} sessions:");
+
+            Console.WriteLine("────────────────────────────────────┬──────────────────────┬──────────┬───────────┬───────────┬───────┬───────┬───────────┬───────────────");
+            Console.WriteLine("Id                                  │            Start Time│     Total│       Scan│        [+]│    [-]│    [A]│      Files│        Folders");
+            Console.WriteLine("────────────────────────────────────┼──────────────────────┼──────────┼───────────┼───────────┼───────┼───────┼───────────┼───────────────");
+
+            for (int index = (statistics.Count - lines); index < statistics.Count; index++)
+            {
+                SessionStatistics stat = statistics[index];
+                TimeSpan scanElapsedTime = TimeSpan.FromTicks(stat.ScanElapsedTime);
+                TimeSpan fileProcessingElapsedTime = TimeSpan.FromTicks(stat.FileProcessingElapsedTime);
+                TimeSpan folderProcessingElapsedTime = TimeSpan.FromTicks(stat.FolderProcessingElapsedTime);
+
+                Console.WriteLine($"{stat.Id}│\t{stat.StartTime.ToString("dd-MM-yyyy HH:mm:ss")}│\t{stat.TotalFiles,6}│\t{scanElapsedTime.TotalSeconds,10:0.0000}│\t{stat.AddedFiles,6}│\t{stat.RemovedFiles,6}│\t{stat.AffectedFolders,6}│\t{fileProcessingElapsedTime.TotalSeconds,10:0.0000}│\t{folderProcessingElapsedTime.TotalSeconds,10:0.0000}");
+            }
+
+            Console.WriteLine("────────────────────────────────────┴──────────────────────┴──────────┴───────────┴───────────┴───────┴───────┴───────────┴───────────────");
+        }
+
+        private static void DisplayHelp(ParserResult<Options> parserResult, IEnumerable<Error> errs)
+        {
+            HelpText helpText = null;
+            if (errs.IsVersion())
+            {
+                helpText = HelpText.AutoBuild(parserResult);
+                Console.WriteLine();
+                Console.WriteLine(helpText);
+            }
+            else
+            {
+                helpText = HelpText.AutoBuild(parserResult, h =>
+                {
+                    h.AdditionalNewLineAfterOption = false;
+                    h.Heading = "NextCloudScan Statistics Viewer 1.0.0.0"; 
+                    h.Copyright = ""; 
+                    return HelpText.DefaultParsingErrorsHandler(parserResult, h);
+                }, e => e);
+                Console.WriteLine($"{Environment.NewLine}{helpText}");
+            }
+        }
+
+        private static void RunOptions(Options obj)
+        {
+            _statsFile = obj.InputFile;
+            _lines = obj.Lines;
+            _summaryOnly = obj.SummaryOnly;
+        }
+
         public static string ToReadableString(TimeSpan span)
         {
             string formatted = string.Format("{0}{1}{2}{3}",
