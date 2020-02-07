277fd3392754662ed18a6d2a384695bdd76ac40e
Nick Bogdanov (work) <konoplinovich@gmail.com>
2020-02-07 16:39:08 +0300

    added actiscloud deploy script

---
+++ deploy_actiscloud.cmd
@@ -0,0 +1,6 @@
+copy NextCloudScan\bin\Release\Extensions.dll n:\nick\files\NextCloudScan\
+copy NextCloudScan\bin\Release\NextCloudScan.exe n:\nick\files\NextCloudScan\
+copy NextCloudScan\bin\Release\NextCloudScanFileLib.dll n:\nick\files\NextCloudScan\
+copy NextCloudScan\bin\Release\NextCloudScanStatLib.dll n:\nick\files\NextCloudScan\
+copy NextCloudScanStatsView\bin\Release\CommandLine.dll n:\nick\files\NextCloudScan\
+copy NextCloudScanStatsView\bin\Release\NextCloudScanStatView.exe n:\nick\files\NextCloudScan\
