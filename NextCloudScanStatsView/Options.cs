b4ce489c0303e90a3ab0b6731e38a120cb6a687c
Nick Bogdanov <konoplinovich@gmail.com>
2020-02-11 20:59:59 +0300

    changed show last n session keyword to "-l, --last"

--- NextCloudScanStatsView/Options.cs
+++ NextCloudScanStatsView/Options.cs
@@ -9,7 +9,7 @@ namespace NextCloudScanStatsView
             [Option('f', "file", Required = true, HelpText = "Statistics XML files to be processed")]
             public string InputFile { get; set; }
 
-            [Option('s', "sessions", Required = false, HelpText = "Show last <n> sessions")]
+            [Option('l', "last", Required = false, HelpText = "Show last <n> sessions")]
             public int Lines { get; set; }
 
             [Option('t', "total", Required = false, HelpText = "Summary only")]
