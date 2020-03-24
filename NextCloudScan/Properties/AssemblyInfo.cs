29c1edd6c1b854e21bb20724c1f9a7962061bb4e
Nick Bogdanov <konoplinovich@gmail.com>
2020-03-24 14:15:04 +0300

    automatic version numbering for debug builds

--- NextCloudScan/Properties/AssemblyInfo.cs
+++ NextCloudScan/Properties/AssemblyInfo.cs
@@ -31,5 +31,10 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
+#if DEBUG
+[assembly: AssemblyVersion("1.5.*")]
+#else
 [assembly: AssemblyVersion("1.5.0.0")]
+#endif
+
 [assembly: AssemblyFileVersion("1.5.0.0")]
